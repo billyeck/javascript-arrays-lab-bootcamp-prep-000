b159fae078b046bb13a6f1705487f6c4132a1c2a
William Eck <billyeck@gmail.com>
2019-04-17 15:35:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(ele){
+  kittens.push(ele)
+  return kittens
+}
+function destructivelyPrependKitten(ele){
+  kittens.shift(ele)
+  return kittens
+}
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+  return kittens
+}
+function destructivelyRemoveFirstKitten(){
+  kittens.unshift()
+  return kittens
+}
+function prependKitten(name){
+  new ary=[name, ...kittens]
+  return ary
+}
+function removeLastKiten(){
+  return kittens.slice(0, kittens.length-1)
+}
+function removeFirstKitten(){
+  return kittens.slice(1)
+}
